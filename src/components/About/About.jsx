512e71228e549576df5a7d1fce37af27639cd6e7
dani <ddctrabajocontacto@gmail.com>
2026-03-19 20:20:12 +0100

    Cambiar foto de About a foto4 y ajustar centrado de imagen

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             <div className="container">
                 <div className="about-grid">
                     <div className="about-image-container">
-                        <img src="/Foto2.PNG" alt="Daniel" className="about-profile-img" />
+                        <img src="/foto4.jpeg" alt="Daniel" className="about-profile-img" />
                     </div>
 
                     <div className="about-text">
